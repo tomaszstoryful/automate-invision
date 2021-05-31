bb802dd58a65e2a86b6ea8ad5e5994902aab7181
Minghua Sun <clairesunstudio@gmail.com>
2021-05-30 21:51:00 -0400

    fix cleanup path

--- cleanup.py
+++ cleanup.py
@@ -4,17 +4,17 @@ import re
 import private
 
 dir_path = private.downloadDir
-list_of_files = [os.path.basename(x) for x in glob.glob(dir_path+'/*')]
-
+list_of_files = [os.path.basename(x) for x in glob.glob(dir_path+'*')]
+print(list_of_files)
 for file_name in list_of_files:
-    match = re.match("^(.*)\s\([\d]+\)(\..*)$", file_name)
+    match = re.match("^(.*)\s\([\d]*\)(\..*)$", file_name)
     file_path = dir_path+file_name
     if match:
         origin = match.group(1)+match.group(2)
         if origin in list_of_files:
-            print(file_name+' removed!!!!')
             os.chmod(file_path, 0o777)
             os.remove(file_path)
+            print(file_name+' removed!!!!')
         else:
             print(file_name+' (kept)')
 
