01b745debb08f3f93a131631e2847fb669c382e8
Minghua Sun <clairesunstudio@gmail.com>
2021-05-30 20:13:40 -0400

    catch exception

--- export.py
+++ export.py
@@ -24,112 +24,118 @@ actions = ActionChains(driver)
 driver.set_window_position(0, 0)
 driver.set_window_size(2000, 1200)
 
-driver.get(URL)
-emailField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_email"]')))
-pwField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_password"]')))
-emailField.send_keys(private.username)
-pwField.send_keys(private.pw)
-submitButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:auth-ui"]/div/div/div[1]/div/div/form/div[3]/button')))
-submitButton.click()
+try:
+    driver.get(URL)
+    emailField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_email"]')))
+    pwField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_password"]')))
+    emailField.send_keys(private.username)
+    pwField.send_keys(private.pw)
+    submitButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:auth-ui"]/div/div/div[1]/div/div/form/div[3]/button')))
+    submitButton.click()
 
-sort = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/span')))
-sort.click()
-sortAZ = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/div/div/ul/li[4]/div/div')))
-sortAZ.click()
+    sort = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/span')))
+    sort.click()
+    sortAZ = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/div/div/ul/li[4]/div/div')))
+    sortAZ.click()
 
-windowHomeName = driver.execute_script("return document.getElementsByTagName('title')[0].text")
-print('window name: '+windowHomeName)
-
-
-
-def exportProject(i):
-    project = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/a')))
-    projectURL = project.get_attribute('href')
-    projectName = project.get_attribute('aria-label')
-    driver.get(projectURL)
-    more = lambda: wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/span/button')))
-    more().click()
-    export = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/button')))
-    export.click()
-    download = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[3]/div/button')))
-    downloadable = download.is_enabled()
-    if downloadable:
-        ##images zip
-        #downloadOption = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[3]/div/button')))
-        ##HTML
-        downloadOption = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[2]/div/button')))
-        ##PDF
-        # downloadOption = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[1]/div/button')))
-        downloadOption.click()
-        #downloadHTML.click()
-        print("downloaded project_"+str(i) +': '+ projectName)
-    else:
-        print("Not downloadable project_"+str(i) +': '+ projectName)
-
-    time.sleep(2)
-    driver.back()
-    time.sleep(2)
-    # switch_to_window to the intial window or switch_to default content both didn't work as the current window is not actually active. This fixed the issue
-    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
+    windowHomeName = driver.execute_script("return document.getElementsByTagName('title')[0].text")
+    print('window name: '+windowHomeName)
 
 
 
-def getProject(i):
-    project =  wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div')))
-    return project
+    def exportProject(i):
+        project = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/a')))
+        projectURL = project.get_attribute('href')
+        projectName = project.get_attribute('aria-label')
+        driver.get(projectURL)
+        more = lambda: wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/span/button')))
+        more().click()
+        export = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/button')))
+        export.click()
+        download = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[3]/div/button')))
+        downloadable = download.is_enabled()
+        if downloadable:
+            ##images zip
+            #downloadOption = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[3]/div/button')))
+            ##HTML
+            downloadOption = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[2]/div/button')))
+            ##PDF
+            # downloadOption = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:prototype-overview"]/div/div/div[1]/div/div[1]/div/section/div[2]/div[1]/div/div/div/ul/li[5]/div/ul/li[1]/div/button')))
+            downloadOption.click()
+            #downloadHTML.click()
+            print("downloaded project_"+str(i) +': '+ projectName)
+        else:
+            print("Not downloadable project_"+str(i) +': '+ projectName)
 
-def getProjectName(i):
-    project = getProject(i)
-    projectName = project.get_attribute('data-title')
-    return projectName
-
-def archiveProject(i):
-    projectMore = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/div/div[2]')))
-    projectMore.click()
-    archiveButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/div/div[2]/div/div/ul/li[4]/div/button')))
-    archiveButton.click()
-    time.sleep(1)
-    confirmButton = driver.find_element_by_xpath('//button[text()="Archive"]')
-    confirmButton.click()
-
-def getProjectType(i):
-    project = getProject(i)
-    projectType = project.get_attribute('data-type')
-    return projectType
-
-exportedProjects = []
-
-def checkProject(i):
-    projectName = getProjectName(i)
-    isPrototype = getProjectType(i) == 'prototype'
-    # only export the projects that has been exported
-    projectNameClean = ''.join(filter(str.isalpha, projectName))
-    projectList = downloads.getFileList(private.downloadDir)
-    if projectNameClean in projectList or projectName in exportedProjects:
-        print(str(i) +': '+ projectName + ' has already been saved.')
-        archiveProject(i)
-    elif isPrototype:
-        exportProject(i)
-        exportedProjects.append(projectName)
-        print(str(i) +': '+ projectName)
-    else:
-        print(str(i) +': '+ projectName + ' skipping not prototype')
-        archiveProject(i)
+        time.sleep(2)
+        driver.back()
+        time.sleep(2)
+        # switch_to_window to the intial window or switch_to default content both didn't work as the current window is not actually active. This fixed the issue
+        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.TAB)
 
 
-# Set the upper bound to at least twice as many as the estimated total counts of the projects.
-for i in range(1, 240):
-    project = lambda: wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(1)+']/div/div/article/a')))
-    projectLink = project()
-    driver.execute_script("arguments[0].focus()", projectLink)
-    time.sleep(1)
-    checkProject(1)
-    driver.get(driver.current_url)
-    time.sleep(2)
-    driver.refresh()
+
+    def getProject(i):
+        project =  wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div')))
+        return project
+
+    def getProjectName(i):
+        project = getProject(i)
+        projectName = project.get_attribute('data-title')
+        return projectName
+
+    def archiveProject(i):
+        projectMore = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/div/div[2]')))
+        projectMore.click()
+        archiveButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/div/div[2]/div/div/ul/li[4]/div/button')))
+        archiveButton.click()
+        time.sleep(1)
+        confirmButton = driver.find_element_by_xpath('//button[text()="Archive"]')
+        confirmButton.click()
+
+    def getProjectType(i):
+        project = getProject(i)
+        projectType = project.get_attribute('data-type')
+        return projectType
+
+    exportedProjects = []
+
+    def checkProject(i):
+        projectName = getProjectName(i)
+        isPrototype = getProjectType(i) == 'prototype'
+        # only export the projects that has been exported
+        projectNameClean = ''.join(filter(str.isalpha, projectName))
+        projectList = downloads.getFileList(private.downloadDir)
+        if projectNameClean in projectList or projectName in exportedProjects:
+            print(str(i) +': '+ projectName + ' has already been saved.')
+            archiveProject(i)
+        elif isPrototype:
+            exportProject(i)
+            exportedProjects.append(projectName)
+            print(str(i) +': '+ projectName)
+        else:
+            print(str(i) +': '+ projectName + ' skipping not prototype')
+            archiveProject(i)
 
 
-print(len(exportedProjects))
-print(exportedProjects)
-print("good bye!")
-driver.close()
+    # Set the upper bound to at least twice as many as the estimated total counts of the projects.
+    for i in range(1, 240):
+        project = lambda: wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(1)+']/div/div/article/a')))
+        projectLink = project()
+        driver.execute_script("arguments[0].focus()", projectLink)
+        time.sleep(1)
+        checkProject(1)
+        driver.get(driver.current_url)
+        time.sleep(2)
+        driver.refresh()
+
+    print(len(exportedProjects))
+    print(exportedProjects)
+    print("good bye!")
+    driver.close()
+
+except TimeoutException as ex:
+    print(len(exportedProjects))
+    print(exportedProjects)
+    print("Exception has been thrown." + str(ex))
+    driver.close()
--- restore.py
+++ restore.py
@@ -25,52 +25,56 @@ actions = ActionChains(driver)
 driver.set_window_position(0, 0)
 driver.set_window_size(2000, 1200)
 
+try:
+    driver.get(URL)
+    emailField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_email"]')))
+    pwField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_password"]')))
+    emailField.send_keys(private.username)
+    pwField.send_keys(private.pw)
+    submitButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:auth-ui"]/div/div/div[1]/div/div/form/div[3]/button')))
+    submitButton.click()
 
-driver.get(URL)
-emailField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_email"]')))
-pwField = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="signin_password"]')))
-emailField.send_keys(private.username)
-pwField.send_keys(user.pw)
-submitButton = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:auth-ui"]/div/div/div[1]/div/div/form/div[3]/button')))
-submitButton.click()
+    sort = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/span')))
+    sort.click()
+    sortAZ = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/div/div/ul/li[4]/div/div')))
+    sortAZ.click()
 
-sort = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/span')))
-sort.click()
-sortAZ = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[3]/div/div/div/div[2]/div/div/div/ul/li[4]/div/div')))
-sortAZ.click()
-
-windowHomeName = driver.execute_script("return document.getElementsByTagName('title')[0].text")
-print('window name: '+windowHomeName)
-
-
-def getProject(i):
-    project =  wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div')))
-    return project
-
-def getProjectName(i):
-    project = getProject(i)
-    projectName = project.get_attribute('data-title')
-    return projectName
-
-def restoreProject(i):
-    print('restoring: '+getProjectName(i))
-    projectMore = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/a')))
-    projectMore.click()
-    time.sleep(1)
-    confirmButton = driver.find_element_by_xpath('//button[text()="Restore"]')
-    confirmButton.click()
+    windowHomeName = driver.execute_script("return document.getElementsByTagName('title')[0].text")
+    print('window name: '+windowHomeName)
 
 
-for i in range(1, 120):
-    project = lambda: wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(1)+']/div/div/article/a')))
-    projectLink = project()
-    driver.execute_script("arguments[0].focus()", projectLink)
-    time.sleep(1)
-    restoreProject(1)
-    time.sleep(1)
-    driver.back()
-    time.sleep(1)
-    driver.refresh()
+    def getProject(i):
+        project =  wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div')))
+        return project
 
-print("good bye!")
-driver.close()
+    def getProjectName(i):
+        project = getProject(i)
+        projectName = project.get_attribute('data-title')
+        return projectName
+
+    def restoreProject(i):
+        print('restoring: '+getProjectName(i))
+        projectMore = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(i)+']/div/div/article/a')))
+        projectMore.click()
+        time.sleep(1)
+        confirmButton = driver.find_element_by_xpath('//button[text()="Restore"]')
+        confirmButton.click()
+
+
+    for i in range(1, 120):
+        project = lambda: wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app-shell:feature-root:home"]/div/section/div[4]/div[4]/div/div/div/div[1]/div/div/div['+str(1)+']/div/div/article/a')))
+        projectLink = project()
+        driver.execute_script("arguments[0].focus()", projectLink)
+        time.sleep(1)
+        restoreProject(1)
+        time.sleep(1)
+        driver.back()
+        time.sleep(1)
+        driver.refresh()
+
+    print("good bye!")
+    driver.close()
+
+except TimeoutException as ex:
+    print("Exception has been thrown." + str(ex))
+    driver.close()
